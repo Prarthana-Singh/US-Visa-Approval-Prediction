0bf7c6a4281639b07805aaa8b0212ae37f3e67e9
Prarthana Singh <ps3493049@gmail.com>
2025-09-01 17:10:25 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version="0.0.0",
+    author="Prarthana",
+    author_email="ps3493049@gmail.com",
+    packages=find_packages()
+)
