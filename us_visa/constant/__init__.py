6405bfadd3949e8f54c2d3d1272909cf2aa5ab45
Prarthana Singh <ps3493049@gmail.com>
2025-09-03 19:25:16 +0530

    Implemented Data Ingestion component and validated by running training pipeline successfully

--- us_visa/constant/__init__.py
+++ us_visa/constant/__init__.py
@@ -1,8 +1,8 @@
 import os
 
 DATABASE_NAME = "US_VISA"
-COLLECTION_NAME = "visa_data"
-MONGODB_URL_KEY = os.getenv("MongoDB_URL")
+COLLECTION_NAME = "VISA_DATA"
+MONGODB_URL_KEY = "MONGODB_URL"
 
 PIPELINE_NAME: str = "usvisa"
 ARTIFACT_DIR: str = "artifact"
@@ -17,7 +17,7 @@ FILE_NAME: str = "usvisa.csv"
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
 
-DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_COLLECTION_NAME: str = "VISA_DATA"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
